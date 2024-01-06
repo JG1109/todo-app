f1cc540e44398c020b72a30d39efae1c2fc6b4ee
Ricky <rickaphile@gmail.com>
2024-01-05 22:46:00 -0800

    Enable local storage for added tasks
    - Add task data to local storage when form is submitted
    - Read from previous local storage every time the app starts
    - Prevent setting the local storage to empty array (which is the initial value of the tasks hook) by checking if tasks dependency is set to empty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import { useState } from "react";
+import { useEffect } from "react";
 import TaskForm from "./TaskForm";
 import Task from "./Task";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks);
+  }, []);
+
   function addTask(name) {
     setTasks((prev) => {
       return [...prev, { name: name, done: false }];
