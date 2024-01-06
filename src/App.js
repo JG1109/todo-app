c2945e8c99e1ebfb2e416e8239d8bfa3e2489cfc
Ricky <rickaphile@gmail.com>
2024-01-05 22:27:27 -0800

    Enable adding and listing tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 import TaskForm from "./TaskForm";
 import Task from "./Task";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  function addTask(name) {
+    setTasks((prev) => {
+      return [...prev, { name: name, done: false }];
+    });
+  }
+
   return (
     <main>
-      <TaskForm />
-      <Task />
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task) => (
+        <Task {...task} />
+      ))}
     </main>
   );
 }
--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Checkbox from "./Checkbox";
 
-function Task() {
+function Task({ name, done }) {
   return (
     <div className="task">
-      <Checkbox defaultChecked={false} />
-      Test task
+      <Checkbox defaultChecked={done} />
+      {name}
     </div>
   );
 }
