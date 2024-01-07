b44e676eb54009fbc9bfa009f59e9674201043f1
Ricky <rickaphile@gmail.com>
2024-01-06 17:43:42 -0800

    Enable tasks to be checked and unchecked
    - Update whether tasks are done or not
    - Trigger the update whenever checkboxes of tasks are clicked

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { useState } from "react";
 
-function Checkbox({ defaultChecked }) {
-  const [checked, setChecked] = useState(defaultChecked);
+function Checkbox({ checked, onClick }) {
   return (
-    <div>
+    <div onClick={onClick}>
       {!checked ? (
         <div className="checkbox unchecked">
           <svg
@@ -13,7 +11,7 @@ function Checkbox({ defaultChecked }) {
             width="14"
             viewBox="0 0 448 512"
           >
-            <path d="M0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96z" />
+            <path d="M384 80c8.8 0 16 7.2 16 16V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V96c0-8.8 7.2-16 16-16H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z" />
           </svg>
         </div>
       ) : (
